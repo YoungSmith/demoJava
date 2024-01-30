b4ca2ccb6b15d2426292d02801b9d5d5142fc146
Tarun Sharma <55961027+YoungSmith@users.noreply.github.com>
2024-01-30 10:16:13 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -3,29 +3,29 @@ import java.util.*;
 public class demo{
     public static void main(String args[]){
         System.out.println("Hello  Jenkins");
-        float a,b;
-        char choice;
-        Scanner sc = new Scanner(System.in);
-        do {
-            System.out.println("1. Add two numbers\n2.Exit\nEnter your Choice\n");
-            choice = sc.next().charAt(0);
-            switch (choice) {
-                case '1':
-                    System.out.println("Enter two number");
-                    a=sc.nextFloat();
-                    b=sc.nextFloat();
-                    float res = (a+b);
-                    System.out.println("Result "+ res);
-                    break;
-                case '2':
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Enter correct input");
-                    break;
-            }
-            System.out.println("\n*****************************************\n");
-        } while (choice!=2);
+        // float a,b;
+        // char choice;
+        // Scanner sc = new Scanner(System.in);
+        // do {
+        //     System.out.println("1. Add two numbers\n2.Exit\nEnter your Choice\n");
+        //     choice = sc.next().charAt(0);
+        //     switch (choice) {
+        //         case '1':
+        //             System.out.println("Enter two number");
+        //             a=sc.nextFloat();
+        //             b=sc.nextFloat();
+        //             float res = (a+b);
+        //             System.out.println("Result "+ res);
+        //             break;
+        //         case '2':
+        //             System.exit(0);
+        //             break;
+        //         default:
+        //             System.out.println("Enter correct input");
+        //             break;
+        //     }
+        //     System.out.println("\n*****************************************\n");
+        // } while (choice!=2);
 
     }
 }
